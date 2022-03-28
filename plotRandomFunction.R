83a03138627644f21d35304925b2e269879485ef
joshr522 <roachjp3@miamioh.edu>
2022-03-28 11:00:12 -0400

    Added some conditional error checking for the sample size n.

--- plotRandomFunction.R
+++ plotRandomFunction.R
@@ -4,6 +4,9 @@
 ##
 ## Josh Roach
 ## March 18, 2022
+##   Amended on March 28, 2022
+##     to do some error checking
+##
 ##
 ## A function that generates 
 ##  a sequence of random normal variables
@@ -17,7 +20,22 @@
 # }
 ## Command+Shift+c comments out selected code
 
+## is.wholenumber() comes from the help documentation
+##   on is.integer -- help(is.integer)
+## It simply checks if a number is an integer or not
+is.wholenumber <- function(x, tol = .Machine$double.eps^0.5) {
+  abs(x-round(x)) < tol
+}
+
 plotRandom <- function(n=1, mu=0, sigma=1, hist_breaks=6) {
-  hist(rnorm(n=n, mean=mu, sd=sigma), breaks=hist_breaks )
+  if(n<0) {
+    stop("Sample size n must be positive integer")
+  }
+  if(!is.wholenumber(n)) {
+    warning(paste("Sample size n should be a whole number\n    R is using n=floor(n)=",
+                  floor(n)))
+  }
+  hist(rnorm(n=n, mean=mu, sd=sigma), 
+       breaks=hist_breaks )
 }
 
--- testingPlotFunction.R
+++ testingPlotFunction.R
@@ -13,6 +13,8 @@ plotRandom(n=60000, mu=70, sigma=2.3)
 
 plotRandom(n=6000, mu=70, sigma=2.3, hist_breaks=30)
 
+plotRandom(n=6000, mu=70, sigma=2.3, hist_breaks=300)
+
 set.seed(20220318)
 plotRandom(n=20)
 
@@ -25,3 +27,15 @@ hist(x, breaks=1)
 hist(x, breaks=3)
 hist(x, breaks=12)
 hist(x, breaks=50)
+
+plotRandom(n=-20)
+plotRandom(n=43.5234)
+## Line 36 works, but what is it doing???
+##   n should be a positive integer
+x <- rnorm(n=43.5234)
+length(x)
+## SO it appears to be truncating
+## Let's do two things to our program
+##   1. Prevent the user from specifying a negative number
+##   2. If the user specifies a positive real number
+##      we should tell them what value is actually being used
